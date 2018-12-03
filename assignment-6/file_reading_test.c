b22bb3be84837e9d307374cdaa339f46a6c07168
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-02 22:47:45 -0500

    debug

--- assignment-6/file_reading_test.c
+++ assignment-6/file_reading_test.c
@@ -34,7 +34,7 @@ int main ( void ){
 			//}
 			
 			int len = strlen(sql_values);
-			sql_values[len-2] = " );";			/* Remove last ", " from string" */
+			sql_values[len-2] = ");";			/* Remove last ", " from string" */
 			
 			//strcat( sql_values, " );" );
 			printf("%s\n", sql_values); 	
