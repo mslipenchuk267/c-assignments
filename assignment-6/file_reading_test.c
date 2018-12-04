46873b9520d2eb63fd495b7001b3b203db2cc245
Matthew Slipenchuk <tuf91673@temple.edu>
2018-12-04 16:28:23 -0500

    debug

--- assignment-6/file_reading_test.c
+++ assignment-6/file_reading_test.c
@@ -49,7 +49,7 @@ int main ( void ){
 
 int tokenIsNumber(char *token) { 
     for (int i = 0; i < s.length(); i++) 
-        if (isdigit(s[i]) == false) 
+        if (isdigit(s[i]) == 0) 
             return 0; /* 0 - false */
   
     return 1; /* 1 - true */
