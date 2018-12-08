07d11c228c02aad7669099ca03aee5fc4b2c272d
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-07 20:54:25 -0500

    test file_read_text with new file

--- assignment-6/file_reading_test.c
+++ assignment-6/file_reading_test.c
@@ -11,7 +11,6 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    return 0;
 }
 
-
 int main ( void ){
 	sqlite3 *db;
 	char *zErrMsg = 0;
---
+++ assignment-6/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include "sqlite3.h"
+
+int main ( void ){
+	create_db("my_database.db");
+	return 1;
+}
