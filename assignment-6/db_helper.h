ce0f6cae89fa0f60e455b70706317e492bd00aa3
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-08 00:22:22 -0500

    linking issues

--- assignment-6/db_helper.h
+++ assignment-6/db_helper.h
@@ -3,7 +3,7 @@
 
 int create_db(char *filename);  /* Creates table for each #-seperated file file */
 int create_tables(char *filename);  /* Creates table for each #-seperated file file */
-int populate_table_from_file(char *filename, char *sql_command);  /* Read data from extentionless files into tables */
+int populate_table_from_file(char *filename);  /* Read data from extentionless files into tables */
 int populate_person_resources(char *filename); /* Use tables to populate person_resources */
 int tokenIsNumber(char *token); /* Check if token from line is number */
 
