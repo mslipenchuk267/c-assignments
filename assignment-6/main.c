0f329ce0d929e40a7fd494fb9c93300c7faab9e9
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-07 21:08:17 -0500

    test file_read_text with new file

--- assignment-6/main.c
+++ assignment-6/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include "sqlite3.h"
 #include "db_helper.h"
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
 
 int main ( void ){
 	create_db("my_database.db");
