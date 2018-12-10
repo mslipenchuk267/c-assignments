808416497dfcee6b46b0e8e2500c63198936555c
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-10 15:40:32 -0500

    test

--- assignment-2/rmcmt.c
+++ assignment-2/rmcmt.c
@@ -12,6 +12,7 @@ int main() {
     int in_comment,first_char = 1;
 	size_t line_number = 1;
 	char output_file[] = "rmcmt_output.c";
+	char *output_file;
     char curr,prev;  /* Store char read from file */
   
     /* Get file name from user. User can provide 
