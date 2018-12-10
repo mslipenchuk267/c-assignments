71e43b7a43a39d01860c03dea53e0d00c587d12d
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-12-09 19:38:39 -0500

    primary key fix

--- assignment-6/read_database.c
+++ assignment-6/read_database.c
@@ -2,17 +2,43 @@
 #include <stdlib.h>
 #include "sqlite3.h" 
 
+static int callback2(void *data, int argc, char **argv, char **azColName);
+
 static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
-   fprintf(stderr, "%s: ", (const char*)data);
-   
-   for(i = 0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   
-   printf("\n");
-   return 0;
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
+
+	for(i = 0; i<argc; i++){
+	  printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+	return 0;
 }
+
+static int callback2(void *data, int argc, char **argv, char **azColName){
+	int i;
+	char result[] = " ";
+	FILE *fp;
+	fp = fopen("person_resources", "w+");
+	//fprintf(fp, "This is testing for fprintf...\n");
+	fputs("This is testing for fputs...\n", fp);
+	fclose(fp);
+
+	fprintf(stderr, "%s: ", (const char*)data);
+	
+	for(i = 0; i<argc; i+=2){
+		strcpt(result,argv[i] ? argv[i] : "NULL");
+		strcat(result,"#");
+		strcat(result,argv[i++] ? argv[i++] : "NULL");
+		fputs(result, fp);
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+	return 0;
+}
+
 
 int main(int argc, char* argv[]) {
    sqlite3 *db;
@@ -40,7 +66,7 @@ int main(int argc, char* argv[]) {
 			"JOIN RESOURCES A ON  A.ID = RR.RESOURCE_ID";
 
    /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   rc = sqlite3_exec(db, sql, callback2, (void*)data, &zErrMsg);
    
    if( rc != SQLITE_OK ) {
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
